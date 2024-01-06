146f41ad3ade4ce43a48c8a434d99c2317834ac9
Sukhostavtseva Katya <KatSuhostav@gmail.com>
2024-01-06 11:59:40 +0300

    Исправлены ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,18 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placeList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function creatingCard(cardName, cardLink, func) {
+function createCard(cardName, cardLink, func) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__image').src = cardLink;
     cardElement.querySelector('.card__title').textContent = cardName;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', func)
-    placeList.append(cardElement);
+    cardElement.querySelector('.card__delete-button').addEventListener('click', func);
+    return cardElement;
 }
 // @todo: Функция удаления карточки
-function deletingCard(event) {
+function deleteCard(event) {
     event.target.parentElement.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(item) {
-    creatingCard(item.name, item.link, deletingCard);
+    placeList.append(createCard(item.name, item.link, deleteCard));
 });
